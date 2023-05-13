a1051af17312f267cb253b0c3f2a3a1abfacadbc
PatilDivyani <125538181+PatilDivyani@users.noreply.github.com>
2023-05-13 19:07:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ const lookup = {
 function rot13(encodedStr){
    let decodedArr = []; // Your Result goes here
   // Only change code below this line
-for(let i = 0; i< encodedStr.length; ++i){
+for(let i = 0; i< encodedStr.length; i++){
 	 if(lookup[ encodedStr[i] ] === undefined){
 		 decodedArr.push(encodedStr[i]);
 	 }
@@ -24,7 +24,9 @@ for(let i = 0; i< encodedStr.length; ++i){
   return decodedArr ;//return decodedArr
 }
 
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
+// You can test your code by running the above function 
+// and printing it to console by pressing the run button at the top.
+// 	To run it with input 36, uncomment the following line
 
 // console.log(rot13("SERR YBIR? NPPVBWBO"));
 
